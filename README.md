c76745ac15bd8c4c03448ba93a3dad5b3be03cfd
daniellzl <mr.daniel.liang@gmail.com>
2017-09-13 16:05:57 -0700

    updated Readme; changed file indentation

--- README.md
+++ README.md
@@ -2,10 +2,37 @@
 
 Displays a list of cooking recipes. Users can add, edit, or delete their own recipes.
 
-## Website
+[Live Application](recipe-box-dlzl.surge.sh)
 
-http://recipe-box-dlzl.surge.sh
+### Project Goals
 
-## Credit:
+Application was completed as a freeCodeCamp [challenge](https://www.freecodecamp.org/challenges/build-a-recipe-box).
 
-https://www.freecodecamp.org/challenges/build-a-recipe-box
+1. User Story: I can create recipes that have names and ingredients.
+
+2. User Story: I can see an index view where the names of all the recipes are visible.
+
+3. User Story: I can click into any of those recipes to view it.
+
+4. User Story: I can edit these recipes.
+
+5. User Story: I can delete these recipes.
+
+6. User Story: All new recipes I add are saved in my browser's local storage. If I refresh the page, these recipes will still be there.
+
+7. Hint:  You should prefix your local storage keys on CodePen, i.e. _username_recipes
+
+### Lessons Learned
+
+* [Make ReactBootstrap labels work in browser](https://stackoverflow.com/questions/33012499/react-bootstrap-fails-to-render)
+* [React and value attribute in input](https://stackoverflow.com/questions/34006333/cant-type-in-react-input-text-field)
+* [Iterate over object in JSX](https://stackoverflow.com/questions/29534224/react-jsx-iterating-through-a-hash-and-returning-jsx-elements-for-each-key)
+* [Store object to state](https://stackoverflow.com/questions/27105257/storing-an-object-in-state-of-a-react-component)
+* [Store to local storage](https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
+
+### Technologies
+
+* babel
+* google fonts
+* react
+* reactbootstrap
--- scripts.js
+++ scripts.js
@@ -18,18 +18,19 @@ const Jumbotron = ReactBootstrap.Jumbotron;
    ██    ██    ██    ███████ ███████
 */
 
+/*
+header title for page
+*/
 const Title = (props) => {
-	// header title for page
-
-    return (
-        <Jumbotron>
-            <h1>
-                <strong>Recipe Box</strong>
-            </h1>
-            <br/>
-            <p>View, Add, and Edit your Favorite Cooking Recipes</p>
-        </Jumbotron>
-    );
+  return (
+    <Jumbotron>
+      <h1>
+        <strong>Recipe Box</strong>
+      </h1>
+      <br/>
+      <p>View, Add, and Edit your Favorite Cooking Recipes</p>
+    </Jumbotron>
+  );
 }
 
 /*
@@ -40,212 +41,199 @@ const Title = (props) => {
 ██   ██ ██████  ██████  ██   ██ ███████  ██████ ██ ██      ███████
 */
 
+/*
+button and form for adding recipes
+*/
 class AddRecipe extends React.Component {
-	// button and form for adding recipes
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            recipeName: "",
-            ingredients: "",
-            showModal: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipeName: "",
+      ingredients: "",
+      showModal: false
+    };
+  }
 
-    close() {
-		// close add recipe form and update complete recipes list
+  // close add recipe form and update complete recipes list
+  close() {
+    this.setState({ showModal: false });
+    this.props.addNewRecipe(this.state.recipeName, this.state.ingredients);
+  }
 
-        this.setState({showModal: false});
-        this.props.addNewRecipe(this.state.recipeName, this.state.ingredients);
-    }
+  // open add recipe form
+  open() {
+    this.setState({ showModal: true });
+  }
 
-    open() {
-		// open add recipe form
-
-        this.setState({showModal: true});
-    }
-
-    render() {
-
-		/*
-        React and value attribute in input
-        https://stackoverflow.com/questions/34006333/cant-type-in-react-input-text-field
-		*/
-        return (
-            <div>
-                <Button
-					bsStyle="info"
-					bsSize="large"
-					onClick={() => this.open()}
-				>
-                    Add Recipe
-                </Button>
-                <Modal
-					show={this.state.showModal} onHide={this.close.bind(this)}
-				>
-                    <Modal.Body>
-                        <div>
-                            <h2>Recipe</h2>
-                            <input
-								type="text"
-								name="recipeName"
-								placeholder="Recipe Name"
-								onChange={(event) => this.setState({recipeName: event.target.value})}
-							/>
-                        </div>
-                        <div>
-                            <h2>Ingredients</h2>
-                            <textarea
-								name="ingredients"
-								rows="10"
-								cols="57"
-								placeholder="Ingredients separated by commas (ex. Apples, Oranges, Bananas)" onChange={(event) => this.setState({ingredients: event.target.value})}
-							/>
-                        </div>
-                        <Button
-							bsStyle="info"
-							onClick={() => this.close()}
-						>
-							Add Recipe
-						</Button>
-                    </Modal.Body>
-                </Modal>
-            </div>
-        );
-    }
-}
-
-/*
-██ ███    ██  ██████  ██████  ███████ ██████  ██      ██ ███████ ████████
-██ ████   ██ ██       ██   ██ ██      ██   ██ ██      ██ ██         ██
-██ ██ ██  ██ ██   ███ ██████  █████   ██   ██ ██      ██ ███████    ██
-██ ██  ██ ██ ██    ██ ██   ██ ██      ██   ██ ██      ██      ██    ██
-██ ██   ████  ██████  ██   ██ ███████ ██████  ███████ ██ ███████    ██
-*/
-
-const IngredientList = (props) => {
-	// convert an ingredients string into a ReactBootstrap list
-
-	// turn ingredients string into array
-    const ingredientArr = props.ingredients.split(",");
-
-	// place each ingredient into its on row
-    const ingredients = ingredientArr.map((ingredient) => {
-        return (
-            <ListGroupItem>
-                {ingredient.trim()}
-            </ListGroupItem>
-        );
-    });
-
+  // React and value attribute in input
+  // https://stackoverflow.com/questions/34006333/cant-type-in-react-input-text-field
+  render() {
     return (
-        <ListGroup>
-            {ingredients}
-        </ListGroup>
+      <div>
+        <Button
+          bsStyle="info"
+          bsSize="large"
+          onClick={() => this.open()}
+        >Add Recipe</Button>
+        <Modal
+          show={this.state.showModal}
+          onHide={this.close.bind(this)}
+        >
+          <Modal.Body>
+            <div>
+              <h2>Recipe</h2>
+              <input
+                type="text"
+                name="recipeName"
+                placeholder="Recipe Name"
+                onChange={(event) => this.setState({recipeName: event.target.value})}
+              />
+            </div>
+            <div>
+              <h2>Ingredients</h2>
+              <textarea
+                name="ingredients"
+                rows="10"
+                cols="57"
+                placeholder="Ingredients separated by commas (ex. Apples, Oranges, Bananas)"
+                onChange={(event) => this.setState({ingredients: event.target.value})}
+              />
+            </div>
+            <Button
+              bsStyle="info"
+              onClick={() => this.close()}
+            >Add Recipe</Button>
+          </Modal.Body>
+        </Modal>
+      </div>
     );
+  }
 }
 
 /*
-██████  ███████  ██████ ██ ██████  ███████ ██      ██ ███████ ████████
-██   ██ ██      ██      ██ ██   ██ ██      ██      ██ ██         ██
-██████  █████   ██      ██ ██████  █████   ██      ██ ███████    ██
-██   ██ ██      ██      ██ ██      ██      ██      ██      ██    ██
-██   ██ ███████  ██████ ██ ██      ███████ ███████ ██ ███████    ██
+██ ███    ██  ██████  ██      ██ ███████ ████████
+██ ████   ██ ██       ██      ██ ██         ██
+██ ██ ██  ██ ██   ███ ██      ██ ███████    ██
+██ ██  ██ ██ ██    ██ ██      ██      ██    ██
+██ ██   ████  ██████  ███████ ██ ███████    ██
 */
 
+/*
+convert an ingredients string into a ReactBootstrap list
+*/
+const IngredientList = (props) => {
+
+  // turn ingredients string into array
+  const ingredientArr = props.ingredients.split(",");
+
+  // place each ingredient into its on row
+  const ingredients = ingredientArr.map((ingredient) => {
+    return (
+      <ListGroupItem>
+        {ingredient.trim()}
+      </ListGroupItem>
+    );
+  });
+
+  return (
+    <ListGroup>
+      {ingredients}
+    </ListGroup>
+  );
+}
+
+/*
+██████  ███████  ██████ ██      ██ ███████ ████████
+██   ██ ██      ██      ██      ██ ██         ██
+██████  █████   ██      ██      ██ ███████    ██
+██   ██ ██      ██      ██      ██      ██    ██
+██   ██ ███████  ██████ ███████ ██ ███████    ██
+*/
+
+/*
+display recipes as a list with ability to edit and delete recipe entries
+*/
 class RecipeList extends React.Component {
-	// display recipes as a list with ability to edit and delete recipe entries
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            recipeName: "",
-            ingredients: "",
-            open: false
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      recipeName: "",
+      ingredients: "",
+      open: false
+    };
+  }
 
+  // opens given recipe edit form
+  open(recipeName) {
+    this.setState({
+      open: !this.state.open,
+      recipeName: recipeName,
+      ingredients: this.props.recipes[recipeName]
+    });
+  }
 
-    open(recipeName) {
-	// opens given recipe edit form
+  // saves recipe edit
+  edit() {
+    this.setState({ open: false });
+    this.props.editRecipe(this.state.recipeName, this.state.ingredients);
+  }
 
-        this.setState({
-            open: !this.state.open,
-            recipeName: recipeName,
-            ingredients: this.props.recipes[recipeName]
-        });
-    }
+  // deletes given recipe
+  delete(recipeName) {
+    this.props.deleteRecipe(recipeName);
+    this.setState({ open: false });
+  }
 
-    edit() {
-	// saves recipe edit
-
-        this.setState({open: false});
-        this.props.editRecipe(this.state.recipeName, this.state.ingredients);
-    }
-
-    delete(recipeName) {
-		// deletes given recipe
-
-        this.props.deleteRecipe(recipeName);
-        this.setState({open: false});
-    }
-
-    render() {
-
-		// create recipe dropdown for a given recipe
-		/*
-        iterate over object in JSX
-        https://stackoverflow.com/questions/29534224/react-jsx-iterating-through-a-hash-and-returning-jsx-elements-for-each-key
-		*/
-        const recipeList = Object.keys(this.props.recipes).map((recipeName, index) => {
-            return (
-                <Panel
+  // create recipe dropdown for a given recipe
+  // iterate over object in JSX
+  // https://stackoverflow.com/questions/29534224/react-jsx-iterating-through-a-hash-and-returning-jsx-elements-for-each-key
+  render() {
+    const recipeList = Object.keys(this.props.recipes).map((recipeName, index) => {
+      return (
+        <Panel
 					header={recipeName}
 					eventKey={index}
 				>
-                    <IngredientList
+          <IngredientList
 						ingredients={this.props.recipes[recipeName]}
 					/>
-                    <Button
+          <Button
 						onClick={() => this.delete(recipeName)} bsStyle="danger"
-					>
-						Delete
-					</Button>
-                    <Button
+					>Delete</Button>
+          <Button
 						onClick={() => this.open(recipeName)} bsStyle="info"
-					>
-						Edit
-					</Button>
-                    <Panel
-						collapsible expanded={this.state.open}>
-                        <div>
-                            <p>Ingredients</p>
-                            <textarea
-								name="ingredients"
-								rows="10"
-								cols="67" defaultValue={this.props.recipes[recipeName]}
-								onChange={(event) => this.setState({ingredients: event.target.value})}
-							/>
-                        </div>
-                        <Button
-							bsStyle="info"
-							onClick={() => this.edit()}
-						>
-							Save Edit
-						</Button>
-                    </Panel>
-                </Panel>
-            );
-        });
-
-        return (
+					>Edit</Button>
+          <Panel
+						collapsible expanded={this.state.open}
+          >
             <div>
-                <Accordion>
-                    {recipeList}
-                </Accordion>
+              <p>Ingredients</p>
+              <textarea
+                name="ingredients"
+                rows="10"
+                cols="67" defaultValue={this.props.recipes[recipeName]}
+                onChange={(event) => this.setState({ingredients: event.target.value})}
+              />
             </div>
-        );
-    }
+            <Button
+              bsStyle="info"
+              onClick={() => this.edit()}
+            >Save Edit</Button>
+          </Panel>
+        </Panel>
+      );
+    });
+
+    return (
+      <div>
+        <Accordion>
+          {recipeList}
+        </Accordion>
+      </div>
+    );
+  }
 }
 
 /*
@@ -256,99 +244,88 @@ class RecipeList extends React.Component {
 ██   ██ ██      ██
 */
 
+/*
+main application
+*/
 class App extends React.Component {
-	// main application
 
-    constructor(props) {
-        super(props);
-        this.state = this.getRecipes();
-		this.addNewRecipe = this.addNewRecipe.bind(this);
-		this.deleteRecipe = this.deleteRecipe.bind(this);
-		this.editRecipe = this.editRecipe.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = this.getRecipes();
+    this.addNewRecipe = this.addNewRecipe.bind(this);
+    this.deleteRecipe = this.deleteRecipe.bind(this);
+    this.editRecipe = this.editRecipe.bind(this);
+  }
 
-	getRecipes() {
-		// gets recipes from local storage if it exists, otherwise returns generic recipes
-
-		const recipes = (localStorage._recipes)
-            ? {
-                recipes: JSON.parse(localStorage._recipes)
-            }
-            : {
-                recipes: {
-                    "Shrimp Francesca": "Shrimp, Artichoke Hearts, Bread Crumbs, Parsley, Lemon, Butter, Garlic, Romano Cheese",
-                    "Chicken Milano": "Butter, Garlic, Sun-Dried Tomatoes, Chicken Broth, Cream, Chicken Breast, Salt, Pepper, Vegetable Oil, Basil, Fettucini Pasta",
-                    "Insalata Caprese": "Tomatoes, Mozzarella Cheese, Leaves, Extra Virgin Olive Oil, Sea Salt, Black Pepper"
-                }
-            };
-
-		return recipes;
-	}
-
-    addNewRecipe(recipeName, ingredients) {
-		// given a new recipe name and list of ingredients, add new recipe to recipes
-		/*
-        store object to state
-        https://stackoverflow.com/questions/27105257/storing-an-object-in-state-of-a-react-component)
-		*/
-		/*
-        store to local storage
-        https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
-		*/
-
-		// if recipe name isn't empty
-        if (recipeName !== "") {
-
-			// add recipe name and ingredient to recipes
-            let recipes = this.state.recipes;
-            recipes[recipeName] = ingredients;
-            this.setState({recipes: recipes});
-
-			// save recipes to local storage
-            localStorage._recipes = JSON.stringify(recipes);
+  // gets recipes from local storage if it exists, otherwise returns generic recipes
+  getRecipes() {
+    const recipes = (localStorage._recipes) ?
+      {recipes: JSON.parse(localStorage._recipes)} :
+      {
+        recipes: {
+          "Shrimp Francesca": "Shrimp, Artichoke Hearts, Bread Crumbs, Parsley, Lemon, Butter, Garlic, Romano Cheese",
+          "Chicken Milano": "Butter, Garlic, Sun-Dried Tomatoes, Chicken Broth, Cream, Chicken Breast, Salt, Pepper, Vegetable Oil, Basil, Fettucini Pasta",
+          "Insalata Caprese": "Tomatoes, Mozzarella Cheese, Leaves, Extra Virgin Olive Oil, Sea Salt, Black Pepper"
         }
+      };
+
+    return recipes;
+  }
+
+  // given a new recipe name and list of ingredients, add new recipe to recipes
+  // store object to state
+  // https://stackoverflow.com/questions/27105257/storing-an-object-in-state-of-a-react-component)
+  // store to local storage
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage)
+  addNewRecipe(recipeName, ingredients) {
+
+    // if recipe name isn't empty
+    if (recipeName !== "") {
+      // add recipe name and ingredient to recipes
+      let recipes = this.state.recipes;
+      recipes[recipeName] = ingredients;
+      this.setState({ recipes: recipes });
+      // save recipes to local storage
+      localStorage._recipes = JSON.stringify(recipes);
     }
+  }
 
-    deleteRecipe(recipeName) {
-		// delete given recipe from recipes
+  // delete given recipe from recipes
+  deleteRecipe(recipeName) {
+    // delete given recipe
+    let recipes = this.state.recipes;
+    delete recipes[recipeName];
+    this.setState({ recipes: recipes });
+    // save recipes to local storage
+    localStorage._recipes = JSON.stringify(recipes);
+  }
 
-		// delete given recipe
-        let recipes = this.state.recipes;
-        delete recipes[recipeName];
-        this.setState({recipes: recipes});
+  // given a existing recipe name and a modified list of ingredients, edit existing recipe's ingredients
+  editRecipe(recipeName, ingredients) {
+    // edit given recipe
+    let recipes = this.state.recipes;
+    recipes[recipeName] = ingredients;
+    this.setState({ recipes: recipes });
+    // save recipes to local storage
+    localStorage._recipes = JSON.stringify(recipes);
+  }
 
-		// save recipes to local storage
-        localStorage._recipes = JSON.stringify(recipes);
-    }
-
-    editRecipe(recipeName, ingredients) {
-		// given a existing recipe name and a modified list of ingredients, edit existing recipe's ingredients
-
-		// edit given recipe
-        let recipes = this.state.recipes;
-        recipes[recipeName] = ingredients;
-        this.setState({recipes: recipes});
-
-		// save recipes to local storage
-        localStorage._recipes = JSON.stringify(recipes);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <Title />
-                <RecipeList
-					recipes={this.state.recipes}
-					deleteRecipe={this.deleteRecipe}
-					editRecipe={this.editRecipe}
-				/>
-                <AddRecipe
-					addNewRecipe={this.addNewRecipe}
-				/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="container">
+        <Title />
+        <RecipeList
+          recipes={this.state.recipes}
+          deleteRecipe={this.deleteRecipe}
+          editRecipe={this.editRecipe}
+        />
+        <AddRecipe
+          addNewRecipe={this.addNewRecipe}
+        />
+      </div>
+    );
+  }
 }
 
 // render application to DOM
-ReactDOM.render( <App/>, document.querySelector("#App"));
+ReactDOM.render(<App/>, document.querySelector("#App"));
